9e0191ebde469e65a73b519ebdd8f6a2d27b9899
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2024-02-11 11:23:14 +0100

    Create agh_dns_rewrites_uni.nix

---
+++ agh_dns_rewrites_uni.nix
@@ -0,0 +1,35 @@
+# List of dns rewrites to use in AdGuardHome
+{
+    services.adguardhome.settings.dns.rewrites = [
+        {   domain = "dns1.uniestro.com";
+            answer = "10.20.30.53"; }
+        {   domain = "dns1";
+            answer = "10.20.30.53"; }
+        {   domain = "dns2.uniestro.com";
+            answer = "10.20.30.35"; }
+        {   domain = "dns2";
+            answer = "10.20.30.35"; }
+        {   domain = "g.uniestro.com";
+            answer = "10.20.30.3"; }
+        {   domain = "g";
+            answer = "10.20.30.3"; }
+        {   domain = "rd.uniestro.com";
+            answer = "10.20.30.3"; }
+        {   domain = "rd";
+            answer = "10.20.30.3"; }
+        {   domain = "secure.uniestro.com";
+            answer = "10.20.30.3"; }
+        {   domain = "secure";
+            answer = "10.20.30.3"; }
+        {   domain = "shell.uniestro.com";
+            answer = "10.20.30.3"; }
+        {   domain = "shell";
+            answer = "10.20.30.3"; }
+        {   domain = "vpn.uniestro.com";
+            answer = "10.20.30.3"; }
+        {   domain = "vpn";
+            answer = "10.20.30.3"; }
+        {   domain = "unifi";
+            answer = "10.20.30.9"; }
+    ];
+}
