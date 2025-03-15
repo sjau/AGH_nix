5620d3c711a74d24251a446e1cbaefe5a6e38709
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2025-03-15 06:25:08 +0100

    Update agh_dns_rewrites_uni.nix

--- agh_dns_rewrites_uni.nix
+++ agh_dns_rewrites_uni.nix
@@ -27,12 +27,14 @@
             answer = "10.20.30.3"; }
         {   domain = "shell";
             answer = "10.20.30.3"; }
+        {   domain = "shell1.uniestro.com";
+            answer = "10.20.30.53"; }
         {   domain = "shell1";
             answer = "10.20.30.53"; }
+        {   domain = "shell2.uniestro.com";
+            answer = "10.20.30.35"; }
         {   domain = "shell2";
             answer = "10.20.30.35"; }
-        {   domain = "shell3";
-            answer = "10.20.30.42"; }
         {   domain = "vpn.uniestro.com";
             answer = "10.20.30.3"; }
         {   domain = "vpn";
