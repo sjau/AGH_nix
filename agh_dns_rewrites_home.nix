4a6fe04bd71902385903ae0765e1079e1616cb94
sjau <github.com_2022.02.04_1643985031@sjau.ch>
2026-05-05 06:31:52 +0200

    Update agh_dns_rewrites_home.nix

--- agh_dns_rewrites_home.nix
+++ agh_dns_rewrites_home.nix
@@ -28,7 +28,7 @@
         {   domain = "mail.jus-law.ch";
             answer = "10.10.10.5";
             enabled = true; }
-        {   domain = "p.jus-law.ch";
+        {   domain = "pdf.jus-law.ch";
             answer = "10.10.10.7";
             enabled = true; }
         {   domain = "shell.jus-law.ch";
